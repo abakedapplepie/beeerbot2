b240ba0ec51a891a6f8486d54305a76f26f1d7ab
abakedapplepie <9055582+abakedapplepie@users.noreply.github.com>
2021-05-08 10:24:36 -0400

    Update comic.py

--- cogs/comic.py
+++ cogs/comic.py
@@ -1,11 +1,17 @@
 import asyncio
+import sys
 import os
+import asyncpraw
+from random import shuffle
+import random
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-import praw
-import random
-from random import shuffle
+import base64
+import requests
+import json
+from datetime import datetime
+from io import BytesIO
 import logging
 import discord
 from discord.ext import commands
@@ -15,100 +21,118 @@ class Comic(commands.Cog):
         self.bot = bot
         self.log = logging.getLogger("beeerbot")
         self.log.info("Comic initialized")
+        self.config = bot.config
+        
+        self.lol = ['mfw', 'dae', 'member', 'tfw', 'lol']
 
     async def cog_command_error(self, ctx, error):
         return
 
     #hook to !comic
     @commands.command()
-    async def comic(self, ctx):
+    async def comic(self, ctx, *arg):
         """comic <title string> - creates a comic and posts it to reddit. title is used for reddit title and imgur title """
-        self.log.info("Comic invoked by %s", ctx.author.name)
-        self.log.info(ctx.message)
-        
-        try:
-            for msg in ctx.history(limit=10,before=ctx.message):
-                self.log.info(msg)
-        except Exception as e:
-            self.log.error(f'Exception encountered', exc_info=1)
+        comic_title = " ".join(arg)
+        try: 
+            self.log.info("Comic invoked by %s, he supplied the title %s", ctx.author.name, comic_title)
 
-        return await ctx.send('testing complete')
-
-        #existing comic code follows, not finished going over
-        for i in range(len(msgs)-1, -1, -1):
-            msg_count += 1
-            diff = msgs[i][0] - msgs[i-1][0]
-            chars.add(msgs[i][1])
-            if msg_count >= bot.config.buffer_size or diff.total_seconds() > bot.config.dead_space or len(chars) > bot.config.total_characters:
-                break
-
-        panels = []
-        panel = []
-
-        seed = random.randint(0, msg_count)
-        i = 0
-        #fix for discord
-        #  for (d, char, msg) in msgs:
-            # if i == seed:
-                # if len(text) > 0:
-                    # reddit_title = text
-                # else:
-                    # reddit_title = msg
-            # if len(panel) == 2 or len(panel) == 1 and panel[0][0] == char:
-                # panels.append(panel)
-                # panel = []
-            # if msg.count('\x01') >= 2:
-                # ctcp = msg.split('\x01', 2)[1].split(' ', 1)
-                # if len(ctcp) == 1:
-                    # ctcp += ['']
-                # if ctcp[0] == 'ACTION':
-                    # msg = '*'+ctcp[1]+'*'
-            # panel.append((char, msg))
-            # i += 1 
-
-        panels.append(panel)
-        self.log.info("Panels are prepared: %s", repr(panels))
-
-        # Initialize a variable to store our image
-        image_comic = BytesIO()
-
-        # Save the completed composition to a JPEG in memory
-        make_comic(chars, panels).save(image_comic, format="JPEG", quality=85)
-
-        # Get API Key, upload the comic to imgur
-        headers = {'Authorization': 'Client-ID ' + bot.config.imgur_client_id}
-        base64img = base64.b64encode(image_comic.getvalue())
-        url = "https://api.imgur.com/3/upload.json"
-        r = requests.post(url, data={'key': bot.config.imgur_client_id, 'image': base64img, 'title': reddit_title}, headers=headers, verify=False)
-        val = json.loads(r.text)
-        try:
-            result = val['data']['link']
+            msgs = []
             try:
-                self.log.info("Authenticating reddit")
-                reddit = praw.Reddit(
-                    client_id=bot.config.reddit_api_id,
-                    client_secret=bot.config.reddit_api_secret,
-                    password=bot.config.reddit_password,
-                    user_agent=bot.config.reddit_agent,
-                    username=bot.config.reddit_username)
-                self.log.info("Authenticated as %s", reddit.user.me())
+                async for msg in ctx.history(limit=15,before=ctx.message):
+                    if not msg.author.bot:
+                        msgs.append(msg)
 
-                try:
-                    submission = reddit.subreddit(bot.config.reddit_subreddit).submit(reddit_title, url=result)
-                    submission_id = submission.id
-                    return await ctx.send("https://redd.it/%s %s %s", submission_id, lol[random.randint(0, lol_count)], reddit_title)
-                except Exception as e:
-                    self.log.error("FAILED to post to reddit - but hey, at least we signed in!", exc_info=1)
-                    return await ctx.send("fail")
             except Exception as e:
-                self.log.error("FAILED to authenticate reddit", exc_info=1)
-                return await ctx.send("fail")
-        except KeyError:
-            self.log.error("FAILED, KeyError", exc_info=1)
-            return await ctx.send("fail")
-        del submission, reddit, r, base64img, image_comic
+                self.log.error(f'Exception encountered', exc_info=1)
+            
+            del msgs[self.config.comic_options.buffer_size:]
+            msg_count = 0
+            chars = set()
+            for i in range(len(msgs)-1, -1, -1):
+                msg_count += 1
+                diff = msgs[i].created_at - msgs[i-1].created_at
+                chars.add(msgs[i].author.name)
+                if msg_count >= self.config.comic_options.buffer_size or diff.total_seconds() > self.config.comic_options.dead_space or len(chars) > self.config.comic_options.total_characters:
+                    self.log.info('breaking out of msg char loop')
+                    break
+            
+            del msgs[msg_count:]
+            msgs.reverse()
 
-    def wrap(st, font, draw, width):
+            panels = []
+            panel = []
+
+            seed = random.randint(0, msg_count)
+            i = 0
+            for msg in msgs:
+                if i == seed:
+                    if len(comic_title) > 0:
+                        reddit_title = comic_title
+                    else:
+                        reddit_title = "{} {}".format(random.choice(self.lol), msg.content)
+                if len(panel) == 2 or len(panel) == 1 and panel[0][0] == msg.author.name:
+                    panels.append(panel)
+                    panel = []
+                panel.append((msg.author.name, msg.content))
+                i += 1 
+
+            panels.append(panel)
+
+            # Initialize a variable to store our image
+            image_comic = BytesIO()
+
+            # Save the completed composition to a JPEG in memory
+            self.make_comic(chars, panels).save(image_comic, format="JPEG", quality=85)
+
+            # Get API Key, upload the comic to imgur
+            headers = {'Authorization': 'Client-ID ' + self.config.api_keys.imgur_client_id}
+            base64img = base64.b64encode(image_comic.getvalue())
+            url = "https://api.imgur.com/3/upload.json"
+            r = requests.post(url, data={'key': self.config.api_keys.imgur_client_id, 'image': base64img, 'title': reddit_title}, headers=headers, verify=False)
+            val = json.loads(r.text)
+            try:
+                imgur_result = val['data']['link']
+                self.log.info(val['data']['link'])
+                try:
+                    self.log.info("Authenticating reddit")
+                    reddit = asyncpraw.Reddit(
+                        client_id=self.config.api_keys.reddit_api_id,
+                        client_secret=self.config.api_keys.reddit_api_secret,
+                        password=self.config.comic_options.reddit_password,
+                        user_agent=self.config.comic_options.reddit_agent,
+                        username=self.config.comic_options.reddit_username)
+                    self.log.info("Authenticated as %s", await reddit.user.me())
+
+                    try:
+                        subreddit = await reddit.subreddit(self.config.comic_options.reddit_subreddit)
+                        submission = await subreddit.submit(reddit_title, url=imgur_result)
+                        submission_id = submission.id
+                    except Exception as e:
+                        self.log.error("FAILED to post to reddit - but hey, at least we signed in!", exc_info=1)
+                        return await ctx.send("fail")
+                except Exception as e:
+                    self.log.error("FAILED to authenticate reddit", exc_info=1)
+                    return await ctx.send("fail")
+            except KeyError:
+                self.log.error("FAILED to upload to imgur", exc_info=1)
+                return await ctx.send("fail")
+
+            em = discord.Embed(
+                title=reddit_title,
+                url="https://redd.it/{}".format(submission_id),
+                description="",
+                color=0xF4B400)
+            em.set_thumbnail(url=imgur_result)
+            em.set_author(name="beeerbot enterprises", icon_url="https://i.imgur.com/UAXu5U7.png")
+            em.set_footer(text="brought to you by raid shadow legends", icon_url="https://i.imgur.com/YKWsNyI.png")
+
+            return await ctx.send(embed=em)
+
+            del submission, reddit, r, base64img, image_comic
+        except  Exception as e:
+            self.log.error("FAILED ", exc_info=1)
+
+    def wrap(self, st, font, draw, width):
         st = st.split()
         mw = 0
         mh = 0
@@ -135,8 +159,7 @@ class Comic(commands.Cog):
 
         return ret, (mw, mh)
 
-
-    def rendertext(st, font, draw, pos):
+    def rendertext(self, st, font, draw, pos):
         ch = pos[1]
         for s in st:
             w, h = draw.textsize(s, font=font)
@@ -147,8 +170,7 @@ class Comic(commands.Cog):
             draw.text((pos[0], ch), s, font=font, fill=(0xff, 0xff, 0xff, 0xff))
             ch += h
 
-
-    def fitimg(img, width, height):
+    def fitimg(self, img, width, height):
         scale1 = float(width) / img.size[0]
         scale2 = float(height) / img.size[1]
 
@@ -162,14 +184,14 @@ class Comic(commands.Cog):
 
         return img.resize((int(l[0]), int(l[1])), Image.ANTIALIAS)
 
+    def make_comic(self, chars, panels):
 
-    def make_comic(chars, panels):
         panelheight = 300
         panelwidth = 450
 
-        filenames = os.listdir('chars/')
+        filenames = os.listdir(self.config.comic_options.char_files)
         shuffle(filenames)
-        filenames = map(lambda x: os.path.join('chars', x), filenames[:len(chars)])
+        filenames = map(lambda x: os.path.join(self.config.comic_options.char_files, x), filenames[:len(chars)])
         chars = list(chars)
         chars = zip(chars, filenames)
         charmap = dict()
@@ -179,10 +201,10 @@ class Comic(commands.Cog):
         imgwidth = panelwidth
         imgheight = panelheight * len(panels)
 
-        bg = Image.open(bot.config.background_file)
+        bg = Image.open(self.config.comic_options.background_file)
 
         im = Image.new("RGB", (imgwidth, imgheight), (0xff, 0xff, 0xff, 0xff))
-        font = ImageFont.truetype(bot.config.font_file, bot.config.font_size)
+        font = ImageFont.truetype(self.config.comic_options.font_file, self.config.comic_options.font_size)
 
         for i in range(len(panels)):
             pim = Image.new("RGB", (panelwidth, panelheight), (0xff, 0xff, 0xff, 0xff))
@@ -194,22 +216,22 @@ class Comic(commands.Cog):
             st2w = 0
             st2h = 0
 
-            (st1, (st1w, st1h)) = wrap(panels[i][0][1], font, draw, 2*panelwidth/3.0)
-            rendertext(st1, font, draw, (10, 10))
+            (st1, (st1w, st1h)) = self.wrap(panels[i][0][1], font, draw, 2*panelwidth/3.0)
+            self.rendertext(st1, font, draw, (10, 10))
             if len(panels[i]) == 2:
-                (st2, (st2w, st2h)) = wrap(panels[i][1][1], font, draw, 2*panelwidth/3.0)
-                rendertext(st2, font, draw, (panelwidth-10-st2w, st1h + 10))
+                (st2, (st2w, st2h)) = self.wrap(panels[i][1][1], font, draw, 2*panelwidth/3.0)
+                self.rendertext(st2, font, draw, (panelwidth-10-st2w, st1h + 10))
 
             texth = st1h + 10
             if st2h > 0:
                 texth += st2h + 10 + 5
 
             maxch = panelheight - texth
-            im1 = fitimg(charmap[panels[i][0][0]], 2*panelwidth/5.0-10, maxch)
+            im1 = self.fitimg(charmap[panels[i][0][0]], 2*panelwidth/5.0-10, maxch)
             pim.paste(im1, (10, panelheight-im1.size[1]), im1)
 
             if len(panels[i]) == 2:
-                im2 = fitimg(charmap[panels[i][1][0]], 2*panelwidth/5.0-10, maxch)
+                im2 = self.fitimg(charmap[panels[i][1][0]], 2*panelwidth/5.0-10, maxch)
                 im2 = im2.transpose(Image.FLIP_LEFT_RIGHT)
                 pim.paste(im2, (panelwidth-im2.size[0]-10, panelheight-im2.size[1]), im2)
 
@@ -218,7 +240,6 @@ class Comic(commands.Cog):
             im.paste(pim, (0, panelheight * i))
 
         return im
-
 
 
 def setup(bot):
