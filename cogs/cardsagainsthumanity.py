009ece2f522deabfbd9951ba486ea86dd4f771b4
Michael Doo <michael.c.doo@gmail.com>
2021-06-08 14:46:42 -0400

    Update CAH

--- cogs/cardsagainsthumanity.py
+++ cogs/cardsagainsthumanity.py
@@ -1,43 +1,47 @@
 import json
-import random
-import os
-import codecs
-import discord
-from discord.ext import commands
 import logging
+import random
 import re
+from typing import Dict, List
+
+from discord.ext import commands
+
 
 class CardsAgainstHumanity(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.log = logging.getLogger("beeerbot")
-        self.log.info("CAH initialized")
+        self.log = logging.getLogger()
         self.config = bot.config
-        
-        self.card_data = {}
-        try:
-            self.card_data.clear()
-            with codecs.open(os.path.join(self.config.bot_options.data_files, "gnomecards.json"), encoding="utf-8") as f:
-                self.card_data.update(json.load(f))
-        except Exception as e:
-            self.log.error('Exception', exc_info=True)
+        self.card_data: Dict[str, List[str]] = {}
+        self.load_cards()
+        self.log.info("CAH initialized")
 
     async def cog_command_error(self, ctx, error):
         return
 
+    def load_cards(self):
+        try:
+            self.card_data.clear()
+            data_file = self.bot.data_path / "gnomecards.json"
+            with data_file.open(encoding="utf-8") as f:
+                self.card_data.update(json.load(f))
+        except Exception as e:
+            self.log.error('Exception', exc_info=True)
+
     @commands.command(aliases=["cah"])
-    async def CAHwhitecard(self, ctx, word):
-        '''Submit text to be used as a CAH whitecard'''
-        return await ctx.send(random.choice(self.card_data['black']).format(word))
+    async def CAHwhitecard(self, ctx, *, text):
+        """<text> - Submit text to be used as a CAH whitecard"""
+        return await ctx.send(random.choice(self.card_data['black']).format(text))
 
     @commands.command(aliases=["cahb"])
     async def CAHblackcard(self, ctx, *, text):
-        '''Submit text with _ for the bot to fill in the rest. You can submit text with multiple _'''
+        """<text> - Submit text with _ for the bot to fill in the rest. You can submit text with multiple _"""
+        CardText = text.strip()
 
         def blankfiller(matchobj):
             return random.choice(self.card_data['white'])
 
-        out = re.sub(r'\b_\b', blankfiller, text)
+        out = re.sub(r"\b_\b", blankfiller, CardText)
         return await ctx.send(out)
 
 def setup(bot):
