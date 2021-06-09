89cedb28b3d4f16e88c90f79f95911cc1546ea18
Michael Doo <michael.c.doo@gmail.com>
2021-06-09 13:25:28 -0400

    Add Stats cog

---
+++ cogs/stats.py
@@ -0,0 +1,402 @@
+from discord.ext import commands, tasks, menus
+from collections import Counter, defaultdict
+
+from .utils import time
+
+import pkg_resources
+import logging
+import discord
+import textwrap
+import datetime
+import traceback
+import itertools
+import asyncio
+import pygit2
+import psutil
+import os
+import re
+import io
+import gc
+
+log = logging.getLogger(__name__)
+
+class GatewayHandler(logging.Handler):
+    def __init__(self, cog):
+        self.cog = cog
+        super().__init__(logging.INFO)
+
+    def filter(self, record):
+        return record.name == 'discord.gateway' or 'Shard ID' in record.msg or 'Websocket closed ' in record.msg
+
+    def emit(self, record):
+        self.cog.add_record(record)
+
+
+def hex_value(arg):
+    return int(arg, base=16)
+
+def object_at(addr):
+    for o in gc.get_objects():
+        if id(o) == addr:
+            return o
+    return None
+
+class Stats(commands.Cog):
+    """Bot usage statistics."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.process = psutil.Process()
+        self._batch_lock = asyncio.Lock(loop=bot.loop)
+        self._data_batch = []
+        self._gateway_queue = asyncio.Queue(loop=bot.loop)
+        self.gateway_worker.start()
+
+    def cog_unload(self):
+        self.gateway_worker.cancel()
+
+    @tasks.loop(seconds=0.0)
+    async def gateway_worker(self):
+        record = await self._gateway_queue.get()
+        await self.notify_gateway_status(record)
+
+    async def register_command(self, ctx):
+        if ctx.command is None:
+            return
+
+        command = ctx.command.qualified_name
+        self.bot.command_stats[command] += 1
+        message = ctx.message
+        destination = None
+        if ctx.guild is None:
+            destination = 'Private Message'
+            guild_id = None
+        else:
+            destination = f'#{message.channel} ({message.guild})'
+            guild_id = ctx.guild.id
+
+        log.info(f'{message.created_at}: {message.author} in {destination}: {message.content}')
+        async with self._batch_lock:
+            self._data_batch.append({
+                'guild': guild_id,
+                'channel': ctx.channel.id,
+                'author': ctx.author.id,
+                'used': message.created_at.isoformat(),
+                'prefix': ctx.prefix,
+                'command': command,
+                'failed': ctx.command_failed,
+            })
+
+    @commands.Cog.listener()
+    async def on_command_completion(self, ctx):
+        await self.register_command(ctx)
+
+    @commands.Cog.listener()
+    async def on_socket_response(self, msg):
+        self.bot.socket_stats[msg.get('t')] += 1
+
+    @discord.utils.cached_property
+    def webhook(self):
+        wh_id = self.bot.config.discord.webhook_id
+        wh_token = self.bot.config.discord.webhook_token
+        hook = discord.Webhook.partial(id=wh_id, token=wh_token, adapter=discord.AsyncWebhookAdapter(self.bot.session))
+        return hook
+
+    async def log_error(self, *, ctx=None, extra=None):
+        e = discord.Embed(title='Error', colour=0xdd5f53)
+        e.description = f'```py\n{traceback.format_exc()}\n```'
+        e.add_field(name='Extra', value=extra, inline=False)
+        e.timestamp = datetime.datetime.utcnow()
+
+        if ctx is not None:
+            fmt = '{0} (ID: {0.id})'
+            author = fmt.format(ctx.author)
+            channel = fmt.format(ctx.channel)
+            guild = 'None' if ctx.guild is None else fmt.format(ctx.guild)
+
+            e.add_field(name='Author', value=author)
+            e.add_field(name='Channel', value=channel)
+            e.add_field(name='Guild', value=guild)
+
+        await self.webhook.send(embed=e)
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def commandstats(self, ctx, limit=20):
+        """Shows command stats.
+
+        Use a negative number for bottom instead of top.
+        This is only for the current session.
+        """
+        counter = self.bot.command_stats
+        width = len(max(counter, key=len))
+        total = sum(counter.values())
+
+        if limit > 0:
+            common = counter.most_common(limit)
+        else:
+            common = counter.most_common()[limit:]
+
+        output = '\n'.join(f'{k:<{width}}: {c}' for k, c in common)
+
+        await ctx.send(f'```\n{output}\n```')
+
+    @commands.command(hidden=True)
+    async def socketstats(self, ctx):
+        delta = datetime.datetime.utcnow() - self.bot.uptime
+        minutes = delta.total_seconds() / 60
+        total = sum(self.bot.socket_stats.values())
+        cpm = total / minutes
+        await ctx.send(f'{total} socket events observed ({cpm:.2f}/minute):\n{self.bot.socket_stats}')
+
+    def get_bot_uptime(self, *, brief=False):
+        return time.human_timedelta(self.bot.uptime, accuracy=None, brief=brief, suffix=False)
+
+    @commands.command()
+    async def uptime(self, ctx):
+        """Tells you how long the bot has been up for."""
+        await ctx.send(f'Uptime: **{self.get_bot_uptime()}**')
+
+    def format_commit(self, commit):
+        short, _, _ = commit.message.partition('\n')
+        short_sha2 = commit.hex[0:6]
+        commit_tz = datetime.timezone(datetime.timedelta(minutes=commit.commit_time_offset))
+        commit_time = datetime.datetime.fromtimestamp(commit.commit_time).astimezone(commit_tz)
+
+        # [`hash`](url) message (offset)
+        offset = time.human_timedelta(commit_time.astimezone(datetime.timezone.utc).replace(tzinfo=None), accuracy=1)
+        return f'[`{short_sha2}`](https://github.com/Rapptz/RoboDanny/commit/{commit.hex}) {short} ({offset})'
+
+    def get_last_commits(self, count=3):
+        repo = pygit2.Repository('.git')
+        commits = list(itertools.islice(repo.walk(repo.head.target, pygit2.GIT_SORT_TOPOLOGICAL), count))
+        return '\n'.join(self.format_commit(c) for c in commits)
+
+    @commands.command()
+    @commands.is_owner()
+    async def about(self, ctx):
+        """Tells you information about the bot itself."""
+
+        revision = self.get_last_commits()
+        embed = discord.Embed(description='Latest Changes:\n' + revision)
+        embed.title = 'Bot Stats'
+        embed.colour = discord.Colour.blurple()
+
+        # To properly cache myself, I need to use the bot support server.
+        support_guild = self.bot.get_guild(self.bot.config.bot_options.guild)
+        log.info(f"self.bot.owner_id: {self.bot.owner_id}; ctx.bot.owner_id: {ctx.bot.owner_id} Support guild: {support_guild}")
+        owner = await self.bot.get_or_fetch_member(support_guild, self.bot.owner_id)
+        embed.set_author(name=str(owner), icon_url=owner.avatar_url)
+
+        # statistics
+        total_members = 0
+        total_unique = len(self.bot.users)
+
+        text = 0
+        voice = 0
+        guilds = 0
+        for guild in self.bot.guilds:
+            guilds += 1
+            if guild.unavailable:
+                continue
+
+            total_members += guild.member_count
+            for channel in guild.channels:
+                if isinstance(channel, discord.TextChannel):
+                    text += 1
+                elif isinstance(channel, discord.VoiceChannel):
+                    voice += 1
+
+        embed.add_field(name='Members', value=f'{total_members} total\n{total_unique} unique')
+        embed.add_field(name='Channels', value=f'{text + voice} total\n{text} text\n{voice} voice')
+
+        memory_usage = self.process.memory_full_info().uss / 1024**2
+        cpu_usage = self.process.cpu_percent() / psutil.cpu_count()
+        embed.add_field(name='Process', value=f'{memory_usage:.2f} MiB\n{cpu_usage:.2f}% CPU')
+
+        version = pkg_resources.get_distribution('discord.py').version
+        embed.add_field(name='Guilds', value=guilds)
+        embed.add_field(name='Commands Run', value=sum(self.bot.command_stats.values()))
+        embed.add_field(name='Uptime', value=self.get_bot_uptime(brief=True))
+        embed.set_footer(text=f'Made with discord.py v{version}', icon_url='http://i.imgur.com/5BFecvA.png')
+        embed.timestamp = datetime.datetime.utcnow()
+        await ctx.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        await self.register_command(ctx)
+        if not isinstance(error, (commands.CommandInvokeError, commands.ConversionError)):
+            return
+
+        error = error.original
+        if isinstance(error, (discord.Forbidden, discord.NotFound, menus.MenuError)):
+            return
+
+        e = discord.Embed(title='Command Error', colour=0xcc3366)
+        e.add_field(name='Name', value=ctx.command.qualified_name)
+        e.add_field(name='Author', value=f'{ctx.author} (ID: {ctx.author.id})')
+
+        fmt = f'Channel: {ctx.channel} (ID: {ctx.channel.id})'
+        if ctx.guild:
+            fmt = f'{fmt}\nGuild: {ctx.guild} (ID: {ctx.guild.id})'
+
+        e.add_field(name='Location', value=fmt, inline=False)
+        e.add_field(name='Content', value=textwrap.shorten(ctx.message.content, width=512))
+
+        exc = ''.join(traceback.format_exception(type(error), error, error.__traceback__, chain=False))
+        e.description = f'```py\n{exc}\n```'
+        e.timestamp = datetime.datetime.utcnow()
+        await self.webhook.send(embed=e)
+
+    def add_record(self, record):
+        # if self.bot.config.debug:
+        #     return
+        self._gateway_queue.put_nowait(record)
+
+    async def notify_gateway_status(self, record):
+        attributes = {
+            'INFO': '\N{INFORMATION SOURCE}',
+            'WARNING': '\N{WARNING SIGN}'
+        }
+
+        emoji = attributes.get(record.levelname, '\N{CROSS MARK}')
+        dt = datetime.datetime.utcfromtimestamp(record.created)
+        msg = textwrap.shorten(f'{emoji} `[{dt:%Y-%m-%d %H:%M:%S}] {record.message}`', width=1990)
+        await self.webhook.send(msg, username='Gateway', avatar_url='https://i.imgur.com/4PnCKB3.png')
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def bothealth(self, ctx):
+        """Various bot health monitoring tools."""
+
+        # This uses a lot of private methods because there is no
+        # clean way of doing this otherwise.
+
+        HEALTHY = discord.Colour(value=0x43B581)
+        UNHEALTHY = discord.Colour(value=0xF04947)
+        WARNING = discord.Colour(value=0xF09E47)
+        total_warnings = 0
+
+        embed = discord.Embed(title='Bot Health Report', colour=HEALTHY)
+
+        description = []
+
+        spam_control = self.bot.spam_control
+        being_spammed = [
+            str(key) for key, value in spam_control._cache.items()
+            if value._tokens == 0
+        ]
+
+        description.append(f'Current Spammers: {", ".join(being_spammed) if being_spammed else "None"}')
+
+        if being_spammed:
+            embed.colour = WARNING
+            total_warnings += 1
+
+        try:
+            task_retriever = asyncio.Task.all_tasks
+        except AttributeError:
+            # future proofing for 3.9 I guess
+            task_retriever = asyncio.all_tasks
+        else:
+            all_tasks = task_retriever(loop=self.bot.loop)
+
+        event_tasks = [
+            t for t in all_tasks
+            if 'Client._run_event' in repr(t) and not t.done()
+        ]
+
+        cogs_directory = os.path.dirname(__file__)
+        tasks_directory = os.path.join('discord', 'ext', 'tasks', '__init__.py')
+        inner_tasks = [
+            t for t in all_tasks
+            if cogs_directory in repr(t) or tasks_directory in repr(t)
+        ]
+
+        bad_inner_tasks = ", ".join(hex(id(t)) for t in inner_tasks if t.done() and t._exception is not None)
+        total_warnings += bool(bad_inner_tasks)
+        embed.add_field(name='Inner Tasks', value=f'Total: {len(inner_tasks)}\nFailed: {bad_inner_tasks or "None"}')
+        embed.add_field(name='Events Waiting', value=f'Total: {len(event_tasks)}', inline=False)
+
+        command_waiters = len(self._data_batch)
+        is_locked = self._batch_lock.locked()
+        description.append(f'Commands Waiting: {command_waiters}, Batch Locked: {is_locked}')
+
+        memory_usage = self.process.memory_full_info().uss / 1024**2
+        cpu_usage = self.process.cpu_percent() / psutil.cpu_count()
+        embed.add_field(name='Process', value=f'{memory_usage:.2f} MiB\n{cpu_usage:.2f}% CPU', inline=False)
+
+        global_rate_limit = not self.bot.http._global_over.is_set()
+        description.append(f'Global Rate Limit: {global_rate_limit}')
+
+        if command_waiters >= 8:
+            total_warnings += 1
+            embed.colour = WARNING
+
+        if global_rate_limit or total_warnings >= 9:
+            embed.colour = UNHEALTHY
+
+        embed.set_footer(text=f'{total_warnings} warning(s)')
+        embed.description = '\n'.join(description)
+        await ctx.send(embed=embed)
+
+    @commands.command(hidden=True, aliases=['cancel_task'])
+    @commands.is_owner()
+    async def debug_task(self, ctx, memory_id: hex_value):
+        """Debug a task by a memory location."""
+        task = object_at(memory_id)
+        if task is None or not isinstance(task, asyncio.Task):
+            return await ctx.send(f'Could not find Task object at {hex(memory_id)}.')
+
+        if ctx.invoked_with == 'cancel_task':
+            task.cancel()
+            return await ctx.send(f'Cancelled task object {task!r}.')
+
+        paginator = commands.Paginator(prefix='```py')
+        fp = io.StringIO()
+        frames = len(task.get_stack())
+        paginator.add_line(f'# Total Frames: {frames}')
+        task.print_stack(file=fp)
+
+        for line in fp.getvalue().splitlines():
+            paginator.add_line(line)
+
+        for page in paginator.pages:
+            await ctx.send(page)
+
+old_on_error = commands.Bot.on_error
+
+async def on_error(self, event, *args, **kwargs):
+    e = discord.Embed(title='Event Error', colour=0xa32952)
+    e.add_field(name='Event', value=event)
+    e.description = f'```py\n{traceback.format_exc()}\n```'
+    e.timestamp = datetime.datetime.utcnow()
+
+    args_str = ['```py']
+    for index, arg in enumerate(args):
+        args_str.append(f'[{index}]: {arg!r}')
+    args_str.append('```')
+    e.add_field(name='Args', value='\n'.join(args_str), inline=False)
+    hook = self.get_cog('Stats').webhook
+    try:
+        await hook.send(embed=e)
+    except:
+        pass
+
+def setup(bot):
+    if not hasattr(bot, 'command_stats'):
+        bot.command_stats = Counter()
+
+    if not hasattr(bot, 'socket_stats'):
+        bot.socket_stats = Counter()
+
+    cog = Stats(bot)
+    bot.add_cog(cog)
+    bot._stats_cog_gateway_handler = handler = GatewayHandler(cog)
+    logging.getLogger().addHandler(handler)
+    commands.Bot.on_error = on_error
+
+def teardown(bot):
+    commands.Bot.on_error = old_on_error
+    logging.getLogger().removeHandler(bot._stats_cog_gateway_handler)
+    del bot._stats_cog_gateway_handler
